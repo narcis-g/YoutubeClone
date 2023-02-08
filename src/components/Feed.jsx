b4e638b7d5c892c1d94bc7189afd3baac17f607a
narcis-g <narcis_alexinho@yahoo.com>
2023-02-08 14:09:45 +0200

    Add some style

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import { Bolt, Stack, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import SideBar from './SideBar';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection: {sx: 'column', md:'row'}}}>
+      <Box sx={{height: {sx: 'auto', md:'92vh'}, borderRight:'1px solid #3d3d3d', px:{sx: 0, md:2}}}>
+        <SideBar></SideBar>
+        <Typography className="copyright" variant='body2' sx={{ marginTop: 1.5, color: '#fff'}}>
+          Copyright 2023 
+        </Typography>
+        
+      </Box>
+    </Stack>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import { Stack } from "@mui/material"
+import { Link } from "react-router-dom"
+import {logo} from '../utilities/constants'
+import SearchBar from "./SearchBar"
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
+const Navbar = () => (
+
+  <Stack direction="row" 
+         alignItems="center" 
+         p={2} 
+         sx={{position: 'sticky', background: "#000", top:5, justifyContent: 'space-between'}}>
+          
+      <Link to='/' style={{display:'flex', alignItems:'center'}}>
+        <img src={logo} alt="logo" height={45}></img>
+      </Link>
+
+      <SearchBar></SearchBar>
+  </Stack>
+  
   )
-}
+
 
 export default Navbar
