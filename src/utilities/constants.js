70bc6d7b7a13dbfa7c2919bc2d290a532f28178d
narcis-g <narcis_alexinho@yahoo.com>
2023-02-09 19:33:15 +0200

    Fix a bug

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -16,6 +16,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
+  { name: 'PewDiePie', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
@@ -33,7 +34,7 @@ export const categories = [
 
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
 export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoVideoUrl ='https://www.youtube.com/@PewDiePie/videos'///video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'PewDiePie';
+export const demoVideoTitle = 'We made history';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
