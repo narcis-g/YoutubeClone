2b9810fa857ce8e04ccb83353fedcc0d9cd7f081
narcis-g <narcis_alexinho@yahoo.com>
2023-02-13 14:20:26 +0200

    Add the VideoDetail functionability

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
-import {Link, useParams} from 'react-router-dom'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Box, Typography, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Video } from "./";
+import { fetchFromAPI } from "../utilities/fetchFromAPI";
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
 
-export default VideoDetail
+  const {id}=useParams()
+  const [videoDetail, setVideoDetail]=useState(null)
+
+  useEffect(() => {
+     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+       .then((data)=> setVideoDetail(data.items[0]))
+  },[id])
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{xs: 'column', md:'row'}}>
+        <Box flex={1}>
+          <Box sx={{width: '100%', position:'sticky', top:'86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
+            className="react-player" controls></ReactPlayer>
+          </Box>
+        </Box>
+      </Stack>
+
+    </Box>
+  )
+};
+
+export default VideoDetail;
